4bdaa05a641e6b5afb305a31bb42a81c63bb3b75
woozzu <woozzu@gmail.com>
2015-07-27 14:57:26 +0900

    Fix matcaffe init_key initialization (https://github.com/BVLC/caffe/pull/2562)

--- matlab/+caffe/private/caffe_.cpp
+++ matlab/+caffe/private/caffe_.cpp
@@ -45,7 +45,7 @@ void mxCHECK_FILE_EXIST(const char* file) {
 static vector<shared_ptr<Solver<float> > > solvers_;
 static vector<shared_ptr<Net<float> > > nets_;
 // init_key is generated at the beginning and everytime you call reset
-static double init_key = static_cast<double>(caffe_rng_rand());
+static double init_key = -2.;
 
 /** -----------------------------------------------------------------
  ** data conversion functions
